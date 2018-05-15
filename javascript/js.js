86a0e41bc2b0a03e1ff4220706643da1eca1c540
Monika Borawska <monikabor20@wp.pl>
2018-05-15 14:42:00 +0200

    Function clear and show points

--- javascript/js.js
+++ javascript/js.js
@@ -1,14 +1,10 @@
+var allPoints = 0;
 var answersBtn = document.querySelectorAll(".answer");
 var checkAnswersBtn = document.querySelector(".check-answer");
-var goodAnswersBtn = document.querySelector(".good-answer");
-var pointsContainer = document.querySelector(".points");
-var pointsSpan = document.querySelector(".points-span");
-var clearBtn = document.querySelector('.clear');
 var showGodAnswersBtn = document.querySelector(".good-answers");
-var startNewGame = document.querySelector(".start-new");
-
-
-var allPoints = 0;
+var pointsContainer = document.querySelector(".points");
+var pointsSpan = document.querySelector(".points span");
+var startNew = document.querySelector(".start-new");
 
 //function add points if answer is true
 function addPoints() {
@@ -20,17 +16,11 @@ function addPoints() {
     }
 }
 
-for (var i = 0; i < answersBtn.length; i++) {
-    answersBtn[i].addEventListener("click", addPoints);
-}
-
 //function show all points
 function showPoints() {
     pointsSpan.innerHTML = allPoints;
     pointsContainer.classList.add("show");
 }
-
-checkAnswersBtn.addEventListener("click", showPoints);
 
 //function show correct answers on green
 function showCorrectAnswers() {
@@ -42,4 +32,23 @@ function showCorrectAnswers() {
     }
 }
 
-showGodAnswersBtn.addEventListener("click", showCorrectAnswers)
+function clearAll() {
+    pointsContainer.classList.remove('show');
+    pointsContainer.classList.add('hide');
+
+    allPoints = 0;
+    for (var i = 0; i < answersBtn.length; i++) {
+        answersBtn[i].classList.remove('selected', 'show-correct');
+    }
+}
+
+
+for (var i = 0; i < answersBtn.length; i++) {
+    answersBtn[i].addEventListener("click", addPoints);
+}
+
+checkAnswersBtn.addEventListener("click", showPoints);
+
+startNew.addEventListener("click", clearAll);
+
+showGodAnswersBtn.addEventListener("click", showCorrectAnswers);
